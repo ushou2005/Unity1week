16c86718c68c0593fb3de3efd40feaa6dcc384fd
symki0227 <58360033+symki0227@users.noreply.github.com>
2023-03-16 13:44:36 +0900

    add GoalPrefab

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float MoveSpeed = 1.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,13 +16,14 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
             transform.position += new Vector3(0, 0, MoveSpeed * Time.deltaTime);
         }
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.RightArrow))
         {
             transform.position += new Vector3(0, 0, -MoveSpeed * Time.deltaTime);
         }
     }
+   
 }
