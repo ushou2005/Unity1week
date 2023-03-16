235e01fc12c146f8660b8d37cb6c1f61b3cc2ebb
symki0227 <58360033+symki0227@users.noreply.github.com>
2023-03-16 14:13:40 +0900

    add[Jump][GoalScropt][Obstacle]

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,9 +5,25 @@ using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float MoveSpeed = 1.0f;
+     [SerializeField] float MoveSpeed = 1.0f;
+     [SerializeField] float JumpPower = 1.0f;
+     [SerializeField] float Timer;
+
+    private Rigidbody rb;
+
+    public enum Stages
+    {
+        stage1,
+        stage2,
+        stage3
+    }
 
     // Start is called before the first frame update
+    private void Awake()
+    {
+        rb = this.GetComponent<Rigidbody>();
+        
+    }
     void Start()
     {
         
@@ -16,6 +32,11 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Timer += Time.deltaTime;
+
+        transform.position += new Vector3(MoveSpeed * Time.deltaTime, 0, 0);
+
+        //Player移動
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             transform.position += new Vector3(0, 0, MoveSpeed * Time.deltaTime);
@@ -24,6 +45,27 @@ public class PlayerMovement : MonoBehaviour
         {
             transform.position += new Vector3(0, 0, -MoveSpeed * Time.deltaTime);
         }
+
+        //ジャンプ
+        if (Input.GetKey(KeyCode.Space))
+            rb.AddForce(0, JumpPower, 0);
+        
     }
-   
+    //障害物にあたった
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Obstacle")
+        {
+            Debug.Log("gameOver");
+        }
+    }
+    //goal
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Goal")
+        {
+            Debug.Log("Clear");
+        }
+    }
+
 }
